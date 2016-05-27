cb3e5a01dae4c7605bdba1d21fd4818d47f6141f
profzoidberg <ryanlynn523@gmail.com>
2016-05-27 17:05:28 -0400

    added hard coded JSON

--- js/app.js
+++ js/app.js
@@ -1,4 +1,46 @@
 $(function(){
+  var show = {
+    seasons: [ 
+      { seasonOne: [
+          episodeOne = {
+            Episode: 1,
+            Title: "Winter Is Coming",
+            Rating: 8.9
+          },
+          episodeTwo = {
+            Episode: 2,
+            Title: "The Kingsroad",
+            Rating: 8.7
+          },
+          episodeThree = {
+            Episode: 3,
+            Title: "Lord Snow",
+            Rating: 8.6
+          }
+        ]
+      },
+      {
+        seasonTwo: [
+          episodeOne = {
+            Episode: 1,
+            Title: "The North Remembers",
+            Rating: 8.7
+          },
+          episodeTwo = {
+            Episode: 2,
+            Title: "The Night Lands",
+            Rating: 8.4
+          },
+          episodeThree = {
+            Episode: 3,
+            Title: "What Is Dead May Never Die",
+            Rating: 8.7
+          }
+        ]
+      }
+    ]
+  }
+  console.log(show);
   $('#search').submit(function (event) {
     event.preventDefault();
     var searchTerm = $('#query').val();
