a91da93357fcc450faef459772eb8b2b991a12b0
profzoidberg <ryanlynn523@gmail.com>
2016-05-27 19:36:53 -0400

    commented out js file for static proto

--- js/app.js
+++ js/app.js
@@ -1,62 +1,19 @@
 $(function(){
-  var show = {
-    seasons: [ 
-      { seasonOne: [
-          episodeOne = {
-            Episode: 1,
-            Title: "Winter Is Coming",
-            Rating: 8.9
-          },
-          episodeTwo = {
-            Episode: 2,
-            Title: "The Kingsroad",
-            Rating: 8.7
-          },
-          episodeThree = {
-            Episode: 3,
-            Title: "Lord Snow",
-            Rating: 8.6
-          }
-        ]
-      },
-      {
-        seasonTwo: [
-          episodeOne = {
-            Episode: 1,
-            Title: "The North Remembers",
-            Rating: 8.7
-          },
-          episodeTwo = {
-            Episode: 2,
-            Title: "The Night Lands",
-            Rating: 8.4
-          },
-          episodeThree = {
-            Episode: 3,
-            Title: "What Is Dead May Never Die",
-            Rating: 8.7
-          }
-        ]
-      }
-    ]
-  }
-  console.log(show);
-  $('#search').submit(function (event) {
+  $('#search').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
     getRequest(searchTerm);
   });
 });
 
-var showResults = function (results) {
+  var showResults = function (results) {
   $('#search-results').append('<p>' + "Episode:" + " " + results.Episode + "," + " " + "Title:" + " " + results.Title + "," +  " " + "Rating:" + " " + results.imdbRating + '</p>');
-}
-
-var getRequest = function (searchTerm) {
-  var params; 
-  var url = 'http://www.omdbapi.com/?';
-  var seasonNum = 3;
-  var hasData = true;
+  }
+  var getRequest = function (searchTerm) {
+    var params; 
+    var url = 'http://www.omdbapi.com/?';
+    var seasonNum = 3;
+    var hasData = true;
   // while(hasData){
     params = {
       t: searchTerm,
